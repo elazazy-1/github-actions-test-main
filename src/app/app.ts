2f9c70c9dcca47540365bb6881289ee6dc8b5335
ahmed343 <ahmed3435535@gmail.com>
2025-12-07 18:33:35 +0200

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('hello-world-app');
+  protected readonly headingColor = signal('black');
+
+  changeColor(): void {
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+    this.headingColor.set(randomColor);
+  }
 }
